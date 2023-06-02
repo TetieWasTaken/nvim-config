ab8d9c1eb08d4985556fbdf52b54931ff3f14953
TetieWasTaken <tjvssr@gmail.com>
2023-06-02 13:43:57 +0200

    chore(settings): add more settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -43,3 +43,21 @@ vim.cmd [[ autocmd VimEnter * silent! Alpha ]]
 
 -- auto close brackets
 require("nvim-autopairs").setup()
+
+-- Vim config
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.wrap = false
+vim.opt.cursorline = true
+vim.opt.showmode = false
+vim.opt.showcmd = false
+vim.opt.showmatch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
